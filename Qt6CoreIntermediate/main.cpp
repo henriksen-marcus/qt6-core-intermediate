d2adfe1aeac253be1a027aa0b5287f8a0613fa63
Marcus Henriksen <89453098+henriksen-marcus@users.noreply.github.com>
2024-12-04 20:22:31 +0100

    u

--- Qt6CoreIntermediate/main.cpp
+++ Qt6CoreIntermediate/main.cpp
@@ -1,19 +1,28 @@
 #include <QCoreApplication>
 #include <iostream>
 #include "test.h"
+#include <QList>
 
-
-void test()
+QSharedPointer<int> test()
 {
-    QScopedPointer<int> sp(new int(5));
-    qInfo() << *sp;
+    QSharedPointer<int> ptr(new int(2));
+    return ptr;
 }
+
+void foo(const QString& s);
 
 int main(int argc, char *argv[])
 {
     //QCoreApplication a(argc, argv);
-
-    test();
-
     //return a.exec();
+
+    QList<int> l;
+    l << 1 << 2 << -1;
+
+    qInfo() << l;
+
+    QMap<int,QString> m;
+    m.value(1);
+
+    foo("hello");
 }
